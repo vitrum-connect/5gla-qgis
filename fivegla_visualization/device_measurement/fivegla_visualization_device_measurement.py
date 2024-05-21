06c371cf8fc42e1579a7e6a41af929323e0854b8
Lukas Lange <ll@vitrum-connect.de>
2024-05-21 17:22:51 +0200

    Refactored MatPlot Code from Felix, changed from one plot to three plots with mean values for 3 depths each, changed data structure

--- fivegla_visualization/device_measurement/fivegla_visualization_device_measurement.py
+++ fivegla_visualization/device_measurement/fivegla_visualization_device_measurement.py
@@ -1,4 +1,10 @@
 from matplotlib.figure import Figure
+
+# felix
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import pandas as pd
+
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from .fivegla_visualization_device_measurement_dialog import FiveGLaVisualizationDeviceMeasurementDialog
 from qgis.PyQt.QtWidgets import QPushButton, QGraphicsScene, QGraphicsView
@@ -35,7 +41,7 @@ class FiveGLaVisualizationDeviceMeasurement:
             self.dlg.btnDraw = self.dlg.findChild(QPushButton, "btnDraw")
             self.dlg.btnClear = self.dlg.findChild(QPushButton, "btnClear")
             self.dlg.plotView = self.dlg.findChild(QGraphicsView, "plotView")
-            self.dlg.btnDraw.clicked.connect(self.draw)
+            self.dlg.btnDraw.clicked.connect(self.draw2)
             self.dlg.btnClear.clicked.connect(self.clear)
             self.first_start = False
             self.dlg.scene = QGraphicsScene()
@@ -50,22 +56,22 @@ class FiveGLaVisualizationDeviceMeasurement:
 
         :return: None
         """
-        figure = Figure()
+        figure = Figure(figsize=(9, 6.5))
         axes = figure.gca()
         axes.set_title('Title')
         # Create a two-dimensional double array with 10 numbers
         # Assume data is an array of tuples, where each tuple is (date, y)
         data = np.array([
-            (datetime.datetime(2022, 1, 1), 1),
-            (datetime.datetime(2022, 1, 2), 2),
-            (datetime.datetime(2022, 1, 3), 3),
-            (datetime.datetime(2022, 1, 4), 4),
-            (datetime.datetime(2022, 1, 5), 5),
-            (datetime.datetime(2022, 1, 6), 6),
-            (datetime.datetime(2022, 1, 7), 7),
-            (datetime.datetime(2022, 1, 8), 8),
-            (datetime.datetime(2022, 1, 9), 9),
-            (datetime.datetime(2022, 1, 10), 10)
+            (datetime.datetime.fromisoformat('2022-01-01T12:00:00'), 1),
+            (datetime.datetime.fromisoformat('2022-01-02T13:00:00'), 2),
+            (datetime.datetime.fromisoformat('2022-01-03T14:00:00'), 3),
+            (datetime.datetime.fromisoformat('2022-01-04T15:00:00'), 4),
+            (datetime.datetime.fromisoformat('2022-01-05T16:00:00'), 5),
+            (datetime.datetime.fromisoformat('2022-01-06T17:00:00'), 6),
+            (datetime.datetime.fromisoformat('2022-01-07T18:00:00'), 7),
+            (datetime.datetime.fromisoformat('2022-01-08T19:00:00'), 8),
+            (datetime.datetime.fromisoformat('2022-01-09T20:00:00'), 9),
+            (datetime.datetime.fromisoformat('2022-01-10T21:00:00'), 10)
         ])
 
         x, y = zip(*data)
@@ -74,7 +80,68 @@ class FiveGLaVisualizationDeviceMeasurement:
         axes.grid()
         axes.plot(x, y, "-k", label="test")
 
+        # Set the limits of x-axis
+        axes.set_xlim([datetime.datetime.fromisoformat('2022-01-01T12:00:00'),
+                       datetime.datetime.fromisoformat('2022-01-15T21:00:00')])
+
+        # Set the x-ticks interval
+        axes.set_xticks(x[::2])
+
+        for label in axes.get_xticklabels():
+            label.set_rotation(90)
+
         canvas = FigureCanvas(figure)
+        proxy_widget = self.dlg.scene.addWidget(canvas)
+        self.dlg.plotView.setScene(self.dlg.scene)
+
+        # self.dlg.plotView.fitInView(proxy_widget)
+        self.dlg.plotView.show()
+
+    def draw2(self):
+        # Spaltennamen für die Messwerte
+        messwert_spalten = ['A1(5)', 'A2(15)', 'A3(25)', 'A4(35)', 'A5(45)', 'A6(55)', 'A7(65)', 'A8(75)', 'A9(85)']
+        # Beschriftungen für die Legende
+        legende_beschriftungen = ['5-25 cm', '35-55 cm', '65-85 cm']
+        data = np.random.rand(10, 9) * 100
+        # Startdatum als numpy datetime64 Objekt
+        start_date = np.datetime64('2022-01-01')
+
+        # Erstellen Sie ein Array von aufeinanderfolgenden Tagen
+        dates = np.arange(start_date, start_date + np.timedelta64(10, 'D'))
+
+        # Mindest- und Maximalwerte über alle Daten ermitteln
+        min_y = np.min(data)
+        max_y = np.max(data)
+
+        num_plots = data.shape[1] // 3
+        fontsize = 20
+        # Subplots erstellen
+        fig, axs = plt.subplots(num_plots, 1, figsize=(15, 20), dpi=400, sharex=True)
+
+        # Prüfen, ob nur eine Achse vorhanden ist, und in eine Liste umwandeln
+        if num_plots == 1:
+            axs = [axs]
+
+        # Gemeinsame beschriftete X-Achse hinzufügen
+        fig.text(0.5, 0.04, 'Datum', ha='center', va='center', fontsize=20)
+        fig.text(0.06, 0.5, 'Bodenfeuchte (%)', ha='center', va='center', rotation='vertical', fontsize=fontsize)
+
+        # Liniendiagramm für jeden Durchschnitt erstellen
+        for i, (start_index, end_index) in enumerate([(0, 3), (3, 6), (6, 9)]):
+            ax = axs[i]
+            depths = data[:, start_index:end_index]
+            avg_depth = np.mean(depths, axis=1)
+            ax.plot(dates, avg_depth, linestyle='-', label=legende_beschriftungen[i])
+            ax.legend(loc='upper left', fontsize=fontsize)
+            ax.grid(True)
+            ax.set_ylim(0, 100)
+            ax.tick_params(axis='y', labelsize=fontsize)
+
+        # X-Achse formatieren
+        axs[-1].xaxis.set_major_formatter(mdates.DateFormatter('%d.%m.%Y'))
+        axs[-1].tick_params(axis='x', rotation=45, labelsize=fontsize)
+
+        canvas = FigureCanvas(fig)
         proxy_widget = self.dlg.scene.addWidget(canvas)
         self.dlg.plotView.setScene(self.dlg.scene)
         self.dlg.plotView.fitInView(proxy_widget)
