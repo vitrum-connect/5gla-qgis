bceaa04086cdb0ecca4fa18744c092b618ee3903
Lukas Lange <86096399+Vitrum-LL@users.noreply.github.com>
2024-05-31 08:23:55 +0200

    Update fivegla_visualization/constants.py
    Co-authored-by: Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>

--- fivegla_visualization/constants.py
+++ fivegla_visualization/constants.py
@@ -5,7 +5,7 @@ class Constants:
     LOG_DIR = PLUGIN_DIR + "/logs"
     DATABASE_CREDENTIALS_FILE = PLUGIN_DIR + "/database_manager/credentials.json"
     SENTEK_SENSOR_TABLE_NAME = "senteksensor"
-    DEVICE_POSITION_TABLE_NAME = "device_position"
+    DEVICE_POSITION_TABLE_NAME = "deviceposition"
 
 
 
